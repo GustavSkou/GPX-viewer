a4422c0892f0de41d382370babb1974fea030a6d
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2025-09-18 16:43:08 +0200

    Improve file input error handling and UI feedback

--- fileInputEventListener.js
+++ fileInputEventListener.js
@@ -1,7 +1,9 @@
 document
   .getElementById("fileInput")
   .addEventListener("change", function (event) {
-    
+
+    removefileErrorMessage();
+
     for (let i = 0; i < event.target.files.length; i++) {
     let file = event.target.files[i];
 
@@ -13,7 +15,7 @@ document
             const fileReader = event.target;
             const route = fileToRoute(fileReader.result, file.name);
             
-            if (MapHandler.instance.maps.has(route.name))
+            if (MapHandler.instance.maps.has(route.name)) // route's "id" shouldn't be the name
               throw new FileError(`${route.name} has already been uploaded`);
 
             let routeDisplayElement = getRouteDisplayTemplate(route.name);
@@ -26,11 +28,11 @@ document
             MapHandler.instance.setViewToRoute(polygon, map);
             MapHandler.instance.setStartPoint(route.points[0].latLngs, map);
             MapHandler.instance.setEndPoint(route.points[route.points.length-1].latLngs, map);
-          } catch (error) {
+          }
+          catch (error) {
             fileErrorHandler(error);
           }
         };
-
         reader.readAsText(file);
       }
     }   
@@ -108,7 +110,17 @@ function addRouteDisplayElementToList(element) {
 function fileErrorHandler(fileError) {
   console.log(fileError.message);
   const error_p = document.getElementById("error_p");
-  error_p.textContent = `${fileError.message} \n ${error_p.textContent}`;
+
+  if (fileError instanceof FileError) {
+    error_p.innerHTML += `${fileError.message}<br>`;
+  } else {
+    error_p.innerHTML += `there was an unexpected error<br>`;
+  }
+}
+
+function removefileErrorMessage() {
+  const error_p = document.getElementById("error_p");
+  error_p.innerHTML = "";
 }
 
 /**
